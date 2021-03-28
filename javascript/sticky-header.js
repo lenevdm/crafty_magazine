c091e1d259368060517d480549b42b3c4bd4eb78
lenevdm <lenebooysen@gmail.com>
2021-03-28 11:35:03 +0200

    Commenting

--- javascript/sticky-header.js
+++ javascript/sticky-header.js
@@ -1,11 +1,14 @@
 /*This script makes the header of the site sticky so that it is always visible when the user scrolls down.*/
 
-window.onscroll = function() {myFunction()};
+/*We check when the user scrolls. When they do, we are going to execute the stickyHeader function.*/
+window.onscroll = function() {stickyHeader()};
 
+/*Here we get the header by assigning the element with the myHeader ID to a variable.*/
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 
-function myFunction() {
+/*The stickyHeader function assigns the sticky class to the header if the page y-offset indicates that the user has scrolled down. */
+function stickyHeader() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
